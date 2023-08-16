43919b7197789f5c5a6d3c32d5bce8b141a0dd3b
ackerman <julio.sergio2709@gmail.com>
2023-08-15 23:08:19 -0400

    mian corrected

--- apps/server/cmd/main.go
+++ apps/server/cmd/main.go
@@ -2,79 +2,60 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io"
+	"errors"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
-
-	"golang.org/x/time/rate"
-
-	"nhooyr.io/websocket"
 )
 
-// echoServer is the WebSocket echo server implementation.
-// It ensures the client speaks the echo subprotocol and
-// only allows one message every 100ms with a 10 message burst.
-type echoServer struct {
-	// logf controls where logs are sent.
-	logf func(f string, v ...interface{})
-}
+func main() {
+	log.SetFlags(0)
 
-func (s echoServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
-		Subprotocols: []string{"echo"},
-	})
+	err := run()
 	if err != nil {
-		s.logf("%v", err)
-		return
-	}
-	defer c.Close(websocket.StatusInternalError, "the sky is falling")
-
-	if c.Subprotocol() != "echo" {
-		c.Close(websocket.StatusPolicyViolation, "client must speak the echo subprotocol")
-		return
-	}
-
-	l := rate.NewLimiter(rate.Every(time.Millisecond*100), 10)
-	for {
-		err = echo(r.Context(), c, l)
-		if websocket.CloseStatus(err) == websocket.StatusNormalClosure {
-			return
-		}
-		if err != nil {
-			s.logf("failed to echo with %v: %v", r.RemoteAddr, err)
-			return
-		}
+		log.Fatal(err)
 	}
 }
 
-// echo reads from the WebSocket connection and then writes
-// the received message back to it.
-// The entire function has 10s to complete.
-func echo(ctx context.Context, c *websocket.Conn, l *rate.Limiter) error {
-	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+// run starts a http.Server for the passed in address
+// with all requests handled by echoServer.
+func run() error {
+	if len(os.Args) < 2 {
+		return errors.New("please provide an address to listen on as the first argument")
+	}
+
+	l, err := net.Listen("tcp", os.Args[1])
+	if err != nil {
+		return err
+	}
+	log.Printf("listening on http://%v", l.Addr())
+
+	s := &http.Server{
+		Handler: echoServer{
+			logf: log.Printf,
+		},
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+	errc := make(chan error, 1)
+	go func() {
+		errc <- s.Serve(l)
+	}()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	select {
+	case err := <-errc:
+		log.Printf("failed to serve: %v", err)
+	case sig := <-sigs:
+		log.Printf("terminating: %v", sig)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	err := l.Wait(ctx)
-	if err != nil {
-		return err
-	}
-
-	typ, r, err := c.Reader(ctx)
-	if err != nil {
-		return err
-	}
-
-	w, err := c.Writer(ctx, typ)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(w, r)
-	if err != nil {
-		return fmt.Errorf("failed to io.Copy: %w", err)
-	}
-
-	err = w.Close()
-	return err
+	return s.Shutdown(ctx)
 }
